194f02a992f4dba7497c40545b13039585904a36
kamesh63 <kameshnaidu639@gmail.com>
2025-02-21 01:05:15 +0530

    Added python casting

---
+++ python_casting.py
@@ -0,0 +1,28 @@
+#Casting
+"""
+Specify a variable type
+There may be times when you want to specify a type on a variable. This can be done with casting. Python is an object-oriented language, and as such it uses classes to define data types, including its primitive types.
+"""
+
+"""
+Casting in python is therefore done using constructor functions
+1.int()
+2.float()
+3.str()
+"""
+
+#Int
+x = int(1) #1
+y = int(2.3) #2
+z = int("3") #3
+
+#Float
+x = float(1) #1.0
+y = float(2.3) #2.3
+z = float("3") #3.0
+a = float("3.0") #3.0
+
+#Str
+x = str(1) #'1'
+y = str(2.3) #'2.3'
+z = str("3") #'3'
